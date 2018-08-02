afebe0bb8331a3e32bfc44ac93d37c5327a4fecb
ghostx94 <ierubiot@gmail.com>
2018-08-02 10:23:07 -0500

    agrego controller para pdf

---
+++ app/Http/Controllers/Encuesta/EncuestaController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Encuesta;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Barryvdh\DomPDF\Facade as PDF;
+
+
+class EncuestaController extends Controller
+{
+    //
+}
